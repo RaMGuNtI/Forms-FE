bfaa22c4c2c2300821ccd07ce024bff7a9303b49
Ram <ramgunti888@gmail.com>
2025-08-19 09:18:38 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://form-be-oao2.onrender.com/';
 
 const instance = axios.create({
   baseURL: API_BASE,
